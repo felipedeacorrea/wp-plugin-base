3c92a759975063250f74861a03bb4f5bb126d971
Felipe de Almeida Correa <facorrea@fcl.com.br>
2022-01-19 01:16:05 -0300

    Arquivo principal para ativação do plugin no WordPress

---
+++ wp-plugin-base.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Base para criação de plugins no WP
+ * Plugin URI: https://felipedeacorrea.com.br
+ * Description: Modelo/Base para criação de plugins no WordPress
+ * Author: Felipe Correa
+ * version: 1.0.0
+ */
+
+namespace NAMESPACEPLUGIN;
+
+require_once __DIR__ . '/vendor/autoload.php';
