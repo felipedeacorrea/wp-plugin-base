fbfdda63e13a13b63bd204d641f5d03dfa146764
Felipe de Almeida Correa <facorrea@fcl.com.br>
2022-01-19 00:53:36 -0300

    Inicio do projeto

---
+++ src/Controllers/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NAMESPACEPLUGIN\Controllers;
+
+abstract class Ecad
+{
+    protected $baseURL = WP_PLUGIN_URL . '/wp-plugin-base';
+    protected $assetsBaseURL = WP_PLUGIN_URL . '/wp-plugin-base/views/assets';
+}
---
+++ src/Helpers/TwigViewer.php
@@ -0,0 +1,26 @@
+<?php
+namespace NAMESPACEPLUGIN\Helpers;
+
+use \Twig\Environment;
+use \Twig\Loader\FilesystemLoader;
+
+class TwigViewer
+{
+    /**
+     * @var string
+     */
+    const VIEWS_PATH = WP_PLUGIN_DIR . '/wp-plugin-base/views';
+
+    /**
+     * @param string $view      Arquivo HTML será renderizado
+     * @param array $content    Conteúdo a ser renderizado no HTML especificado
+     * @return string           HTML tratado com as variaveis previamente substituidas
+     */
+    public static function render($view, $content)
+    {
+        $loader = new FilesystemLoader(self::VIEWS_PATH);
+        $twig = new Environment($loader);
+
+        return $twig->render($view, $content);
+    }
+}
